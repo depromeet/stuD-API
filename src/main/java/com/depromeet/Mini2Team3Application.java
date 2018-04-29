2303543c0a2f5d1a921013d6a6e6ddf58a400614
BeINone <sbeinone@gmail.com>
2018-04-29 22:23:06 +0900

    프로젝트 생성 및 초기 설정
    - Spring Boot 프로젝트 생성
    - Maven dependency 설정

---
+++ src/main/java/com/depromeet/Mini2Team3Application.java
@@ -0,0 +1,12 @@
+package com.depromeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mini2Team3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mini2Team3Application.class, args);
+	}
+}
