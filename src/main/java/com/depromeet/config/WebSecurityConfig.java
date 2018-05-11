f975de7857b2e419d6f0144ae0923dcd4d86543d
BeINone <sbeinone@gmail.com>
2018-05-11 21:55:43 +0900

    CORS 적용

--- src/main/java/com/depromeet/config/WebSecurityConfig.java
+++ src/main/java/com/depromeet/config/WebSecurityConfig.java
@@ -30,6 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 			.httpBasic().disable()
+			.cors().and()
 			.authorizeRequests()
 			.antMatchers("/").permitAll()
 			.antMatchers(HttpMethod.POST, "/login").permitAll()
